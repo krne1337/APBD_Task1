a0a114ec94fc4ce8f00bd011511ca09ca4164559
s20764 <s20764@pjwstk.edu.pl>
2024-03-24 21:34:25 +0100

    message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,11 +19,32 @@ namespace ConsoleApp1
 
             return (double)sum / numbers.Length;
         }
+        public static int FindMaximum(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Array must not be null or empty");
+            }
+
+            int max = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            return max;
+        }
         static void Main(string[] args)
         {
             int[] array = { 1, 2, 3, 4, 5 };
             double average = CalculateAverage(array);
             Console.WriteLine("Average: " + average);
+
+            int maximum = FindMaximum(array);
+            Console.WriteLine("Maximum value: " + maximum);
 
             Console.WriteLine("Hello World!");
             Console.WriteLine("Mod1");
