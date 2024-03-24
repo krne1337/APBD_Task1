4d7bff3d3a99500c8dfda0a8a00ac78dcafc573b
s20764 <s20764@pjwstk.edu.pl>
2024-03-24 21:31:10 +0100

    message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,8 +4,27 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        public static double CalculateAverage(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Array must not be null or empty");
+            }
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            return (double)sum / numbers.Length;
+        }
         static void Main(string[] args)
         {
+            int[] array = { 1, 2, 3, 4, 5 };
+            double average = CalculateAverage(array);
+            Console.WriteLine("Average: " + average);
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("Mod1");
             Console.WriteLine("Mod2");
